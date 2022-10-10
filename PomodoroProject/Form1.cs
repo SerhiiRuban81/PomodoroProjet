32a108b9a71219153adcc8e4576c4ed820780e26
SerhiiRuban81 <serhii.ruban@ukr.net>
2022-10-10 20:03:16 +0300

    SayHello added

--- PomodoroProject/Form1.cs
+++ PomodoroProject/Form1.cs
@@ -6,5 +6,10 @@ namespace PomodoroProject
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
     }
 }
